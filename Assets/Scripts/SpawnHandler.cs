9dc16dc066ee7486a30cd5dc40c1dfcd1604efc8
unknown <apanchenko@APPDEV-PC-4.am.local>
2015-12-10 19:08:43 +0300

    Simple SpawnHandler

---
+++ Assets/Scripts/SpawnHandler.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace JimRunner
+{
+    public class SpawnHandler : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject[] stageCollection;
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.tag == "SpawnTrigger")
+            {
+                var stage = other.gameObject;
+                Transform spawnLocation = stage.transform.parent.Find("SpawnLocation");
+
+                GameObject obj = Instantiate(stageCollection[(Random.Range(0, stageCollection.Length))],
+                    spawnLocation.position,
+                    Quaternion.identity) as GameObject;
+
+                obj.name = "SpawnedStage";
+            }
+
+            if(other.gameObject.name == "SpawnLocation")
+            {
+                Destroy(other.gameObject.transform.parent.gameObject);
+            }
+        }
+    }
+}
