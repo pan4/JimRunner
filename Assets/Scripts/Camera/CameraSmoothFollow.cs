73d9bc020e18dc6b3bf2200bcdc28e482133c440
Alexander Panchenko <a.panch90@gmail.com>
2015-12-10 21:30:30 +0300

    camrea smooth follow

---
+++ Assets/Scripts/Camera/CameraSmoothFollow.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace JimRunner
+{
+    public class CameraSmoothFollow : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform target;
+        [SerializeField]
+        private Vector3 offset = Vector3.zero;
+        [SerializeField]
+        private float damping = 1;
+        [SerializeField]
+        private float lookAheadFactor = 3;
+        [SerializeField]
+        private float lookAheadReturnSpeed = 0.5f;
+        [SerializeField]
+        private float lookAheadMoveThreshold = 0.1f;
+        
+
+
+        private float m_OffsetZ;
+        private Vector3 m_LastTargetPosition;
+        private Vector3 m_CurrentVelocity;
+        private Vector3 m_LookAheadPos;
+
+        // Use this for initialization
+        private void Start()
+        {
+            m_LastTargetPosition = target.position + offset;
+            m_OffsetZ = (transform.position - target.position).z;
+            transform.parent = null;
+        }
+
+
+        // Update is called once per frame
+        private void Update()
+        {
+            // only update lookahead pos if accelerating or changed direction
+            float xMoveDelta = (target.position + offset - m_LastTargetPosition).x;
+
+            bool updateLookAheadTarget = Mathf.Abs(xMoveDelta) > lookAheadMoveThreshold;
+
+            if (updateLookAheadTarget)
+            {
+                m_LookAheadPos = lookAheadFactor * Vector3.right * Mathf.Sign(xMoveDelta);
+            }
+            else
+            {
+                m_LookAheadPos = Vector3.MoveTowards(m_LookAheadPos, Vector3.zero, Time.deltaTime * lookAheadReturnSpeed);
+            }
+
+            Vector3 aheadTargetPos = target.position + offset + m_LookAheadPos + Vector3.forward * m_OffsetZ;
+            Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref m_CurrentVelocity, damping);
+
+            transform.position = newPos;
+
+            m_LastTargetPosition = target.position + offset;
+        }
+    }
+}
