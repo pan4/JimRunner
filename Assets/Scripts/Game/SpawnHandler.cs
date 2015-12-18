76004bc4542b4db21cd982d3a4861c15ce12fd02
unknown <apanchenko@APPDEV-PC-4.am.local>
2015-12-18 18:13:24 +0300

    endless day disign upd

--- Assets/Scripts/Game/SpawnHandler.cs
+++ Assets/Scripts/Game/SpawnHandler.cs
@@ -5,33 +5,79 @@ namespace JimRunner
 {
     public class SpawnHandler : MonoBehaviour
     {
+        private const string SpawnLocation = "SpawnLocation";
+
         [SerializeField]
         private GameObject[] stageCollection;
 
         [SerializeField]
         private GameObject mainClouds;
 
+        [SerializeField]
+        private GameObject firstRock;
+
+        [SerializeField]
+        private GameObject clouds;
+
+        [SerializeField]
+        private GameObject secondRocks;
+
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (other.gameObject.tag == "PlatformSpawnTrigger")
             {
                 var stage = other.gameObject;
-                Transform spawnLocation = stage.transform.parent.Find("PlatformSpawnLocation");
+                Transform spawnLocation = stage.transform.parent.Find(SpawnLocation);
 
                 GameObject obj = Instantiate(stageCollection[(Random.Range(0, stageCollection.Length))],
                     spawnLocation.position,
                     Quaternion.identity) as GameObject;
 
-                obj.name = "SpawnedStage";
+                obj.name = "Platform";
+                var root = stage.transform.parent.transform.parent;
+                obj.transform.parent = root;
             }
 
             if (other.gameObject.tag == "MainCloudSpawnTrigger")
             {
                 var stage = other.gameObject;
-                Transform spawnLocation = stage.transform.parent.Find("MainCloudSpawnLocation");
+                Transform spawnLocation = stage.transform.parent.Find(SpawnLocation);
 
                 GameObject obj = Instantiate(mainClouds, spawnLocation.position, Quaternion.identity) as GameObject;
-                obj.name = "SpawnedMainCloud";
+                obj.name = "MainCloud";
+                var root = stage.transform.parent.transform.parent;
+                obj.transform.parent = root;
+            }
+
+            if (other.gameObject.tag == "FirstRockSpawnTrigger")
+            {
+                var stage = other.gameObject;
+                Transform spawnLocation = stage.transform.parent.Find(SpawnLocation);
+
+                GameObject obj = Instantiate(firstRock, spawnLocation.position, Quaternion.identity) as GameObject;
+                obj.name = "FirstRock";
+                var root = stage.transform.parent.transform.parent;
+                obj.transform.parent = root;
+            }
+
+            if (other.gameObject.tag == "CloudSpawnTrigger")
+            {
+                var stage = other.gameObject;
+                Transform spawnLocation = stage.transform.parent.Find(SpawnLocation);
+
+                GameObject obj = Instantiate(clouds, spawnLocation.position, Quaternion.identity) as GameObject;
+                obj.name = "Cloud";
+                var root = stage.transform.parent.transform.parent;
+                obj.transform.parent = root;
+            }
+
+            if (other.gameObject.tag == "SecondRockSpawnTrigger")
+            {
+                var stage = other.gameObject;
+                Transform spawnLocation = stage.transform.parent.Find(SpawnLocation);
+
+                GameObject obj = Instantiate(secondRocks, spawnLocation.position, Quaternion.identity) as GameObject;
+                obj.name = "SecondRock";
                 var root = stage.transform.parent.transform.parent;
                 obj.transform.parent = root;
             }
