922d1986a2c979841652c6b5142ab12028f13b30
Alexander Panchenko <a.panch90@gmail.com>
2015-12-18 23:24:16 +0300

    stone shadow

--- Assets/Scripts/Game/StoneController.cs
+++ Assets/Scripts/Game/StoneController.cs
@@ -5,26 +5,35 @@ namespace JimRunner
 {
     public class StoneController : MonoBehaviour
     {
+        [SerializeField]
+        Transform _rotation;
+
+        [SerializeField]
+        Transform _shadow;
+
         Transform _transform;
 
         [SerializeField]
         float _speed;
 
         private float _rotateSpeed;
+        private Vector3 _shadowOffset;
 
         void Start()
         {
             _transform = transform;
-            CircleCollider2D circleCollider = GetComponent<CircleCollider2D>();
+            CircleCollider2D circleCollider = _rotation.GetComponent<CircleCollider2D>();
             _rotateSpeed = (360 * _speed) / (2 * Mathf.PI * circleCollider.radius * circleCollider.transform.localScale.x);
-            
+            _shadowOffset = _shadow.localPosition; 
         }
 
         private void Update()
         {
             //Debug.Log(_rotateSpeed);
-            _transform.Translate(new Vector3(-_speed * Time.deltaTime, 0f, 0f), Space.World);
-            _transform.Rotate(0f, 0f, _rotateSpeed * Time.deltaTime);
+            //_transform.Translate(new Vector3(-_speed * Time.deltaTime, 0f, 0f), Space.World);
+            _rotation.Rotate(0f, 0f, _rotateSpeed * Time.deltaTime);
+            _rotation.Translate(new Vector3(-_speed * Time.deltaTime, 0f, 0f), Space.World);
+            _shadow.position = _rotation.position + _shadowOffset;
         }
 
     }
