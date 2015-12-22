c7d633cd135f6f04f1db6d00d17e8fe39c33af08
unknown <apanchenko@APPDEV-PC-4.am.local>
2015-12-22 16:51:43 +0300

    CameraFollowTest

---
+++ Assets/Scripts/Camera/CameraFollowTest.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowTest : MonoBehaviour {
+
+    public GameObject player;
+    public Camera main_camera;
+
+    void Update()
+    {
+        if (player != null)
+        {
+            float player_x = player.transform.position.x;
+            float player_y = player.transform.position.y;
+
+            float rounded_x = RoundToNearestPixel(player_x);
+            float rounded_y = RoundToNearestPixel(player_y);
+
+            Vector3 new_pos = new Vector3(rounded_x, rounded_y, -10.0f); // this is 2d, so my camera is that far from the screen.
+            main_camera.transform.position = new_pos;
+        }
+    }
+    public float pixelToUnits = 40f;
+
+    public float RoundToNearestPixel(float unityUnits)
+    {
+        float valueInPixels = unityUnits * pixelToUnits;
+        valueInPixels = Mathf.Round(valueInPixels);
+        float roundedUnityUnits = valueInPixels * (1 / pixelToUnits);
+        return roundedUnityUnits;
+    }
+}
