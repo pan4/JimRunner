d1ac4e82d8d113ad41288cbc7ec2c5ad93643f56
Alexander Panchenko <a.panch90@gmail.com>
2015-12-23 21:40:42 +0300

    evnening, day-evening spritsheets

--- Assets/Scripts/Camera/CameraSmoothFollow.cs
+++ Assets/Scripts/Camera/CameraSmoothFollow.cs
@@ -25,12 +25,17 @@ namespace JimRunner
         private Vector3 m_CurrentVelocity;
         private Vector3 m_LookAheadPos;
 
+        [SerializeField]
+        private int pixelToUnits = 100;
+
         // Use this for initialization
         private void Start()
         {
             m_LastTargetPosition = target.position + offset;
             m_OffsetZ = (transform.position - target.position).z;
             transform.parent = null;
+
+            Application.targetFrameRate = 20;
         }
 
 
@@ -54,9 +59,22 @@ namespace JimRunner
             Vector3 aheadTargetPos = target.position + offset + m_LookAheadPos /*+ Vector3.forward * m_OffsetZ*/;
             Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref m_CurrentVelocity, damping);
 
+            //float rounded_x = RoundToNearestPixel(newPos.x);
+            //float rounded_y = RoundToNearestPixel(newPos.y);
+
+            //transform.position = new Vector3(rounded_x, Mathf.Clamp(rounded_y, 3f, 4.2f), newPos.z);
             transform.position = new Vector3(newPos.x, Mathf.Clamp(newPos.y, 3f, 4.2f), newPos.z);
 
             m_LastTargetPosition = target.position /*+ offset*/;
         }
+
+        public float RoundToNearestPixel(float unityUnits)
+        {
+            float valueInPixels = unityUnits * pixelToUnits;
+            valueInPixels = Mathf.Round(valueInPixels);
+            float roundedUnityUnits = valueInPixels  / pixelToUnits;
+            return roundedUnityUnits;
+        }
     }
+
 }
